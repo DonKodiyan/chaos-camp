d1416bc6ecb69e63904928a4371d36b86c46899a
Don Kodiyan <don.kodiyan@gmail.com>
2019-03-30 18:50:45 +0100

    doko: use spring webflux

--- app/src/main/java/ch/zuehlke/chaoscamp/app/AppBackendApplication.java
+++ app/src/main/java/ch/zuehlke/chaoscamp/app/AppBackendApplication.java
@@ -2,10 +2,10 @@ package ch.zuehlke.chaoscamp.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
 @SpringBootApplication
-@EnableSwagger2
+@EnableSwagger2WebFlux
 public class AppBackendApplication {
 
   public static void main(String[] args) {
