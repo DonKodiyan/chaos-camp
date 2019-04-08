3f03daef98fcc62b108e14393f9b8be5246fc69a
Flepp, Simon <Simon.Flepp@zuehlke.com>
2019-04-08 15:01:30 +0300

    Add cpu-intensive and memory-intensive endpoints

--- app/src/main/java/ch/zuehlke/chaoscamp/app/AppController.java
+++ app/src/main/java/ch/zuehlke/chaoscamp/app/AppController.java
@@ -1,6 +1,7 @@
 package ch.zuehlke.chaoscamp.app;
 
 import io.micrometer.core.annotation.Timed;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,47 +13,77 @@ import reactor.core.publisher.Mono;
 @RestController
 public class AppController {
 
-  private final WebClient webClient;
+    private final WebClient webClient;
 
-  public AppController(@Value("${external.hasher.api}") String api) {
-    webClient = WebClient.create(api);
+    public AppController(@Value("${external.hasher.api}") String api) {
+        webClient = WebClient.builder()
+                .baseUrl(api)
+                .build();
 
-  }
-
-  @Timed(value="api.hello")
-  @GetMapping("api/hello")
-  public Mono<String> hash(@RequestParam("value") String value) {
-    return webClient
-            .get()
-            .uri(uriBuilder -> uriBuilder.queryParam("value", value).build())
-            .accept(MediaType.APPLICATION_JSON_UTF8)
-            .retrieve()
-            .bodyToMono(Response.class)
-            .map(Response::getResult);
-  }
-
-  @Timed(value="api.info")
-  @GetMapping("api/info")
-  public Mono<String> info() {
-    return Mono.just(
-            "<br/> Node MY_NODE_NAME: " + System.getenv().get("MY_NODE_NAME") +
-            "<br/> Node MY_POD_NAME: " + System.getenv().get("MY_POD_NAME") +
-            "<br/> Node MY_POD_NAMESPACE: " + System.getenv().get("MY_POD_NAMESPACE") +
-            "<br/> Node MY_POD_IP: " + System.getenv().get("MY_POD_IP") +
-            "<br/> Node MY_POD_SERVICE_ACCOUNT: " + System.getenv().get("MY_POD_SERVICE_ACCOUNT")
-    ) ;
-  }
-
-  public static class Response {
-
-    private String result;
-
-    public String getResult() {
-      return result;
     }
 
-    public void setResult(String result) {
-      this.result = result;
+    @Timed(value = "api.hello")
+    @GetMapping("api/hello")
+    public Mono<String> hash(@RequestParam("value") String value) {
+        return webClient
+                .get()
+                .uri(uriBuilder -> uriBuilder.path("/hash").queryParam("value", value).build())
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .retrieve()
+                .bodyToMono(Response.class)
+                .map(Response::getResult);
     }
-  }
+
+    @Timed(value = "api.info")
+    @GetMapping("api/info")
+    public Mono<String> info() {
+        return Mono.just(
+                "<br/> Node MY_NODE_NAME: " + System.getenv().get("MY_NODE_NAME") +
+                        "<br/> Node MY_POD_NAME: " + System.getenv().get("MY_POD_NAME") +
+                        "<br/> Node MY_POD_NAMESPACE: " + System.getenv().get("MY_POD_NAMESPACE") +
+                        "<br/> Node MY_POD_IP: " + System.getenv().get("MY_POD_IP") +
+                        "<br/> Node MY_POD_SERVICE_ACCOUNT: " + System.getenv().get("MY_POD_SERVICE_ACCOUNT")
+        );
+    }
+
+    @Timed(value = "api.cpu-intensive")
+    @GetMapping("api/cpu-intensive")
+    public Mono<String> getCpuIntensiveTask() {
+        String value = RandomStringUtils.randomAlphabetic(10);
+
+        return webClient
+                .get()
+                .uri(uriBuilder -> uriBuilder.path("/hash").queryParam("value", value).build())
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .retrieve()
+                .bodyToMono(Response.class)
+                .map(Response::getResult);
+    }
+
+    @Timed(value = "api.memory-intensive")
+    @GetMapping("api/memory-intensive")
+    public Mono<String> getMemoryIntensiveTask() {
+        String value = RandomStringUtils.randomAlphabetic(10);
+
+        return webClient
+                .get()
+                .uri(uriBuilder -> uriBuilder.path("/hash-from-memory").queryParam("value", value).build())
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .retrieve()
+                .bodyToMono(Response.class)
+                .map(Response::getResult);
+    }
+
+    public static class Response {
+
+        private String result;
+
+        public String getResult() {
+            return result;
+        }
+
+        public void setResult(String result) {
+            this.result = result;
+        }
+    }
 }
--- hasher/src/main/java/ch/zuehlke/chaoscamp/hasher/HasherController.java
+++ hasher/src/main/java/ch/zuehlke/chaoscamp/hasher/HasherController.java
@@ -6,25 +6,55 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.util.HashMap;
+
 @RestController
 public class HasherController {
 
-  @GetMapping("api/hash")
-  public Mono<Response> hash(@RequestParam("value") String value) {
-    return Mono.just(new Response(BCrypt.hashpw(value, BCrypt.gensalt(10))));
-  }
+    private HashMap<String, String> inMemoryValues = new HashMap<>();
 
-  public static class Response {
-
-    private String result;
-
-    public Response(String result) {
-      this.result = result;
+    /**
+     * Calculates the hash for a provided value. CPU-intensive
+     *
+     * @param value
+     * @return
+     */
+    @GetMapping("api/hash")
+    public Mono<Response> hashCpu(@RequestParam("value") String value) {
+        return Mono.just(new Response(hash(value)));
     }
 
-    public String getResult() {
-      return result;
+    /**
+     * Calculates or gets the hash from a in-memory cache. Memory-intensive
+     *
+     * @param value
+     * @return
+     */
+    @GetMapping("api/hash-from-memory")
+    public Mono<Response> hashFromMemory(@RequestParam("value") String value) {
+        if (!inMemoryValues.containsKey(value)) {
+            inMemoryValues.put(value, hash(value));
+        }
+
+        return Mono.just(new Response(inMemoryValues.get(value)));
     }
-  }
+
+
+    private String hash(String value) {
+        return BCrypt.hashpw(value, BCrypt.gensalt(10));
+    }
+
+    public static class Response {
+
+        private String result;
+
+        public Response(String result) {
+            this.result = result;
+        }
+
+        public String getResult() {
+            return result;
+        }
+    }
 
 }
